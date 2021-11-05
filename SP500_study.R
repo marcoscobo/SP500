d751c0df5bbe7ad832681c45af0cbda0208b7c3f
marcoscobo <marcoscobocarrillo@gmail.com>
2021-11-05 22:58:50 +0100

    sapply at simulations and empirical probs

--- SP500_study.R
+++ SP500_study.R
@@ -1,72 +1,44 @@
-library(fitdistrplus) # "norm", "lnorm", "pois", "exp", "gamma", "nbinom", "geom", "beta", "unif", "logis"
+## Part I
+
 source("simulations.R")
 
 df <- read.csv("SP500.csv")
 
-rent <- hist.rent(df, verbose=T)
-sprintf('Rentabilidad anual media = %4f', (rent - 1) * 100)
+years.to.sim <- 25
+number.of.sim <- 5000
 
-years.to.sim <- 20
+anual.rents <- simulate(df, years.to.sim, number.of.sim, plot=F)
+
+hist(anual.rents, breaks=seq(min(anual.rents) - 0.01, max(anual.rents) + 0.01, by=0.01))
+
+
+
+## Part II
+
+number.of.sim <- 1000000
+anual.rents <- simulate.apply(df, years.to.sim, number.of.sim, paral=T)
+
+
+x11(); par(mfrow = c(2, 1))
+hist(anual.rents, breaks=seq(min(anual.rents) - 0.005, max(anual.rents) + 0.005, by=0.005), prob=T)
+lines(density(anual.rents), col='red')
+plot(ecdf(anual.rents), col='red', col.01line=NULL)
+
+
+calc.prob <- function(x, simuls){
+  return(length(simuls[simuls <= x]) / length(simuls))
+}
+
+prob1 <- calc.prob(1, anual.rents)
+sprintf('Probabilidad de rent <= 1 a %i años: %4f', years.to.sim, prob1)
+prob2 <- 1 - calc.prob(1.05, anual.rents)
+sprintf('Probabilidad de rent > 1.05 a %i años: %4f', years.to.sim, prob2)
+
+
 number.of.sim <- 10000
-
-anual.rents <- simulate(df, years.to.sim, number.of.sim, plot.n=T, plot.l=T)
-
-png(filename = 'rents.png', width=1920, height=1080)
-hist(anual.rents, breaks=seq(min(anual.rents) - 0.01, max(anual.rents) + 0.1, by=0.01))
-dev.off()
-
-## DISTRIBUCION DE LAS RENTABILIDADES
-
-shapiro.test(anual.rents)
-
-fn <- fitdist(anual.rents, "norm")
-fl <- fitdist(anual.rents, "logis")
-
-x11()
-par(mfrow = c(1, 2))
-plot.legend <- c("norm", "logis")
-denscomp(list(fn, fl), legendtext = plot.legend)
-qqcomp(list(fn, fl), legendtext = plot.legend)
-cdfcomp(list(fn, fl), legendtext = plot.legend)
-ppcomp(list(fn, fl), legendtext = plot.legend)
-
-
-## CALCULO DE PROBABILIDADES
-sprintf('Rentabilidad anual media: %4f', mean(anual.rents))
-
-# normal
-nprob <- pnorm(1, mean=fn$estimate[1], sd=fn$estimate[2])
-sprintf('Probabilidad de rent <= 1 a %i años: %4f', years.to.sim, nprob)
-
-nprob2 <- (pnorm(1.14, mean=fn$estimate[1], sd=fn$estimate[2]) - pnorm(1.06, mean=fn$estimate[1], sd=fn$estimate[2]))
-sprintf('Probabilidad de rent en (1.06,1.14) a %i años: %4f', years.to.sim, nprob2)
-
-nprob3 <- (1 - pnorm(1.05, mean=fn$estimate[1], sd=fn$estimate[2]))
-sprintf('Probabilidad de rent > 1.05 a %i años: %4f', years.to.sim, nprob3)
-
-
-# logistic
-lprob <- plogis(1, location=fl$estimate[1], scale=fl$estimate[2])
-sprintf('Probabilidad de rent <= 1 a %i años: %4f', years.to.sim, lprob)
-
-lprob2 <- (plogis(1.14, location=fl$estimate[1], scale=fl$estimate[2]) - plogis(1.06, location=fl$estimate[1], scale=fl$estimate[2]))
-sprintf('Probabilidad de rent en (1.06,1.14) a %i años: %4f', years.to.sim, lprob2)
-
-lprob3 <- (1 - plogis(1.05, location=fl$estimate[1], scale=fl$estimate[2]))
-sprintf('Probabilidad de rent > 1.05 a %i años: %4f', years.to.sim, lprob3)
-
-## PLOT
-
 probs <- c()
-for (year in 1:50){
-  anual.rents <- simulate(df, year, 5000)
-  fl <- fitdist(anual.rents, "logis")
-  probs <- c(probs, plogis(1, location=fl$estimate[1], scale=fl$estimate[2]))
+for (year in 1:60){
+  anual.rents <- simulate.apply(df, year, number.of.sim, paral=T)
+  probs <- c(probs, calc.prob(1, anual.rents))
 }
-
-for (year in seq(5, 50, by=5)){
-  print(probs[year])
-}
-
-x11()
 plot(probs, type='l')
--- simulations.R
+++ simulations.R
@@ -1,53 +1,45 @@
-simulate <- function(df, years.to.sim, number.of.sim, save=F, plot.n=F, plot.l=F, cero.line=T){
+## Part I
+
+simulate <- function(df, years.to.sim, number.of.sim, plot=F){
   
   total.days <- length(df$Date)
   days.to.sim <- 252 * years.to.sim
   last.init <- total.days - days.to.sim
   
-  simuls <- matrix(data=NA, nrow=days.to.sim, ncol=number.of.sim) 
+  if (plot == T){
+    simuls <- matrix(data=NA, nrow=days.to.sim, ncol=number.of.sim) 
+  }
+  
+  anual.rents <- c()
   for (i in 1:number.of.sim){
     init.day <- sample(1:last.init, size=1) + 1
     last.day <- init.day + days.to.sim - 1
     sim.period <- init.day:last.day
     sim.sample <- sample(sim.period, replace=T)
     sim.diff <- df$Diff_close_div[sim.sample]
-    simuls[,i] <- cumprod(sim.diff)
-  }
-  rents <- simuls[days.to.sim,]
-  anual.rents <- rents ^ (1 / years.to.sim)
-  
-  
-  
-  if (save == T){
-    write.csv(data.frame(simuls), 'simulations.csv', row.names = FALSE)
+    anual.rent <- prod(sim.diff) ** (1 / years.to.sim)
+    anual.rents <- c(anual.rents, anual.rent)
+    
+    if (plot == T){
+      simuls[,i] <- cumprod(sim.diff)
+    }
   }
   
-  if (plot.n == T){
-    #png(filename = 'graf_n.png', width=1920, height=1080)
-    #x11()
+  if (plot == T){
+    x11()
     plot(simuls[,1], type='l', ylim=c(min(simuls), max(simuls)))
     for (i in 2:number.of.sim){
       rgb <- sample(50:255, size=3)/255
       lines(simuls[,i], col=rgb(rgb[1], rgb[2], rgb[3]))
     }
-    if (cero.line == T){
-      abline(h=1, lwd=2, col='red')
-    }
-    #dev.off()
-  }
-  
-  if (plot.l == T){
-    #png(filename = 'graf_l.png', width=1920, height=1080)
-    #x11()
+    abline(h=1, lwd=2, col='red')
+    x11()
     plot(log(simuls[,1]), type='l', ylim=c(min(log(simuls)), max(log(simuls))))
     for (i in 2:number.of.sim){
       rgb <- sample(1:255, size=3)/255
       lines(log(simuls[,i]), col=rgb(rgb[1], rgb[2], rgb[3]))
     }
-    if (cero.line == T){
-      abline(h=0, lwd=2, col='red')
-    }
-    #dev.off()
+    abline(h=0, lwd=2, col='red')
   }
   
   return(anual.rents)
@@ -55,10 +47,35 @@ simulate <- function(df, years.to.sim, number.of.sim, save=F, plot.n=F, plot.l=F
 
 
 
-hist.rent <- function(df, verbose=F){
+## Part II
+
+library(parallel)
+
+sim.apply <- function(x, df, years.to.sim){
   
-  years <- length(df$Close) / 252
-  rent <- df$Cum_Diff_close_div[length(df$Cum_Diff_close_div)] ^ (1 / years)
+  total.days <- length(df$Date)
+  days.to.sim <- 252 * years.to.sim
+  last.init <- total.days - days.to.sim
   
-  return(rent)
+  init.day <- sample(1:last.init, size=1) + 1
+  last.day <- init.day + days.to.sim - 1
+  sim.period <- init.day:last.day
+  sim.sample <- sample(sim.period, replace=T)
+  sim.diff <- df$Diff_close_div[sim.sample]
+  anual.rent <- prod(sim.diff) ** (1 / years.to.sim)
+  
+  return(anual.rent)
+}
+
+simulate.apply <- function(df, years.to.sim, number.of.sim, paral=F){
+  
+  if (paral == T){
+    cl <- makeCluster(detectCores() - 2)
+    anual.rents <- parSapply(cl=cl, X=1:number.of.sim, FUN=sim.apply, df, years.to.sim)
+    stopCluster(cl)
+  } else {
+    anual.rents <- sapply(X=1:number.of.sim, FUN=sim.apply, df, years.to.sim)
+  }
+  
+  return(anual.rents)
 }
